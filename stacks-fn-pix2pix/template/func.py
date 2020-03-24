473389c5fe4a3cf4855d97bea1d21564e153d796
mmauck <milan.mauck@intel.com>
2020-03-24 09:08:12 -0700

    Pix2Pix usecase working!

--- stacks-fn-pix2pix/template/func.py
+++ stacks-fn-pix2pix/template/func.py
@@ -1,23 +1,23 @@
 import io
 import json
-
+from binascii import a2b_base64
+import base64
 
 from fdk import response
 from infer import infer
 
 def handler(ctx, data: io.BytesIO=None):
-
-    infer('0.jpg')
-
-    name = "worked!"
+    inputImage = None
+    outputImage = None
     try:
         body = json.loads(data.getvalue())
-        name = body.get("name")
+        imageAsBase64String = body.get("imageAsBase64String")
+        # Turn the base64 input into a file object
+        inputImage = io.BytesIO(base64.b64decode(imageAsBase64String))
+        # Run inference on the image
+        outputImage = infer(inputImage)
+
     except (Exception, ValueError) as ex:
         print(str(ex))
 
-    return response.Response(
-        ctx, response_data=json.dumps(
-            {"message": "It {0}".format(name)}),
-        headers={"Content-Type": "application/json"}
-    )
+    return response.Response(ctx, response_data=json.dumps({"output": str(outputImage, 'ascii')}),headers={"Content-Type": "application/json"})
