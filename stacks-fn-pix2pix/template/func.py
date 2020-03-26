bafed1d210b166d64d274b3fb0c079554c98b778
mmauck <milan.mauck@intel.com>
2020-03-26 14:45:27 -0700

    You can input image files and README updated

--- stacks-fn-pix2pix/template/func.py
+++ stacks-fn-pix2pix/template/func.py
@@ -10,14 +10,17 @@ def handler(ctx, data: io.BytesIO=None):
     inputImage = None
     outputImage = None
     try:
-        body = json.loads(data.getvalue())
-        imageAsBase64String = body.get("imageAsBase64String")
+        #body = json.loads(data.getvalue())
+        #imageAsBase64String = body.get("imageAsBase64String")
+        imageAsBase64String = data.getvalue()
         # Turn the base64 input into a file object
-        inputImage = io.BytesIO(base64.b64decode(imageAsBase64String))
+        #inputImage = io.BytesIO(base64.b64decode(imageAsBase64String))
+        inputImage = io.BytesIO(imageAsBase64String)
         # Run inference on the image
         outputImage = infer(inputImage)
 
     except (Exception, ValueError) as ex:
         print(str(ex))
 
-    return response.Response(ctx, response_data=json.dumps({"output": str(outputImage, 'ascii')}),headers={"Content-Type": "application/json"})
+    return response.Response(ctx, response_data=str(outputImage, 'ascii'),headers={"Content-Type": "text/html"})
+    #return response.Response(ctx, response_data=json.dumps({"output": str(outputImage, 'ascii')}),headers={"Content-Type": "application/json"})
